2e9eb51f17f4c860e2ccd712e047d5cfe3d7a25b
unknown <liponbdbd@gmail.com>
2026-03-05 22:03:05 +0600

    created isPalindrome

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
-const array = [3, 8, 2, 5, 7, 6, 12];
-let current = 0; // current window sum
-let minSum = Infinity; // minimum sum
-const k = 4; // window size
-
-// Step 1: Calculate sum of first window
-for (let i = 0; i < k; i++) {
-    current += array[i];
+for (var i = 0; i < 3; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 1000);
 }
-
-minSum = current;
-
-// Step 2: Slide the window
-for (let i = 1; i <= array.length - k; i++) {
-    // remove left element, add right element
-    current = current - array[i - 1] + array[i + k - 1];
-
-    if (current < minSum) {
-        minSum = current;
-    }
-}
-
-console.log('minSum', minSum);
